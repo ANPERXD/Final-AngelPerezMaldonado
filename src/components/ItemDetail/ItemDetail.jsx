36a1ad2d3ff15f6e2bbabe31f1616f992f2dd5a5
ANPERXD <angeljoelperez02@gmail.com>
2023-09-17 14:16:17 -0300

    Vers 0.3

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import ItemCount from "../ItemCount/ItemCount";
+import { Link } from "react-router-dom";
 
 const ItemDetail = ({ id, name, img, category, description, price, stock }) => {
+    const [quantityAdded, setQuantityAdded] = useState(0)
+    const handleOnAdd = (quantity) => {
+        setQuantityAdded(quantity)
+    }
     return (
         <article className="CardItem">
             <header className="Header">
@@ -23,7 +29,14 @@ const ItemDetail = ({ id, name, img, category, description, price, stock }) => {
                 </p>
             </section>
             <footer className="ItemFooter">
-                <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log('Cantidad Agregada', quantity)} />
+                {
+                    quantityAdded > 0 ? (
+                        <Link to='/cart' className='Option'>Terminar Compra</Link>
+                    ) : (
+                        <ItemCount initial={1} stock={stock} onAdd={handleOnAdd} />
+                    )
+                }
+
             </footer>
         </article>
     )
