26f52535eca5f96e02ad6f150b49f95fde69a46b
ANPERXD <angeljoelperez02@gmail.com>
2023-09-18 10:44:30 -0300

    Vers 0.4

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
-          <NavBar />
-          <Routes>
-            <Route path='/' element={<ItemListContainer />} />
-            <Route path='/category/:categoryId' element={<ItemListContainer />} />
-            <Route path='/item/:itemId' element={<ItemDetailContainer />} />
-            <Route path='*' element={<h1>404 NOT FOUND</h1>} />
-          </Routes>
+          <CartProvider>
+            <NavBar />
+            <Routes>
+              <Route path='/' element={<ItemListContainer greeting={'Todos nuestros productos'} />} />
+              <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Productos Por categoria'} />} />
+              <Route path='/item/:itemId' element={<ItemDetailContainer />} />
+              <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+            </Routes>
+          </CartProvider>
         </BrowserRouter>
       </header>
     </div>
